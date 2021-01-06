3ff26eac4bbcb1fe6287a5664014851164062e4d
yuvalchen7@gmail.com <150397>
2021-01-06 13:39:44 +0200

    king

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          זה נראה לי די מגניב
+         היום אני מלך
         </p>
       </header>
     </div>
